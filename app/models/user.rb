8e8a044440e1bcec25669a19badf63b918dc7723
Roxanne Cojocariu <rmcojocariu@gmail.com>
2018-12-30 17:17:29 -0500

    add shelter model and user associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  belongs_to :Shelter
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true
